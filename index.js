a69d737613c7a1afd67ec6ac97333d180940af8e
Shin <Shin8@users.noreply.github.com>
2017-03-20 19:47:27 -0600

    Base page

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('body').fadeIn(200, function(){
+    $('.navbar').delay(500).fadeIn(200, function(){
+      $('.content').delay(500).fadeIn(200, function(){});
+    });
+  });
+});
